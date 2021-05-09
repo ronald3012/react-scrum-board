52a12982e832471229756817ccb474cd0da9fefa
Ronald De Leon <301294r@gmail.com>
2021-05-09 14:17:08 -0400

    Update style

--- src/Components/AddColumn.js
+++ src/Components/AddColumn.js
@@ -14,6 +14,7 @@ const AddColumn = ({ onAddNewColumn }) => {
         onSubmit={(e) => {
           e.preventDefault();
           onAddNewColumn(columnName);
+          setColumnName('');
         }}
       >
         <input
